37e71bf3c8adffea067e43e3b92073eab0eafad6
Rishu Chauhan <rishuchauhan>
2024-08-27 22:33:46 +0530

    SellTheStock

---
+++ localRepo/BuyAndSell.java
@@ -0,0 +1,15 @@
+public class BuyAndSell {
+    public static int sellStock(int arr[]){
+        int profit=0;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>arr[i-1]){
+                profit +=(arr[i]-arr[i-1]);
+            }
+        }
+        return profit;
+    }
+   public static void main(String []args){
+    int arr[]={7,1,5,3,6,4};
+    System.out.println(sellStock(arr));
+   } 
+}
